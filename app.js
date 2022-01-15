5ec111a3418151d12bdf883923711d286a469c0c
Severyn Hundiak <severyn.h@gmail.com>
2022-01-15 07:54:05 +0200

    Make js for imageSwitch

--- app.js
+++
@@ -1,8 +0,0 @@
-function myFunction() {
-    var x = document.getElementById("nav-bar");
-    if (x.style.display === "block") {
-        x.style.display = "none";
-    } else {
-        x.style.display = "block";
-    }
-}
---
+++ script.js
@@ -0,0 +1,42 @@
+function menu() {
+    const x = document.getElementById("nav-bar");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+
+function imageSwitchLeft(element) {
+    const item = element.parentElement.parentElement;
+    const image = item.querySelector('img');
+    const url = image.getAttribute('src');
+    const arr = url.split('').reverse();
+    const pictureNumber = Number(arr[4]);
+    arr[4] = (pictureNumber - 1).toString();
+    newUrl = arr.slice().reverse().join('');
+    if (Number(arr[4]) < 1) {
+        arr[4] = '6';
+        newUrl = arr.slice().reverse().join('');
+        image.setAttribute('src', newUrl);
+    } else {
+        image.setAttribute('src', newUrl);
+    }
+}
+
+function imageSwitchRight(element) {
+    const item = element.parentElement.parentElement;
+    const image = item.querySelector('img');
+    const url = image.getAttribute('src');
+    const arr = url.split('').reverse();
+    const pictureNumber = Number(arr[4]);
+    arr[4] = (pictureNumber + 1).toString();
+    newUrl = arr.slice().reverse().join('');
+    if (Number(arr[4]) > 6) {
+        arr[4] = '1';
+        newUrl = arr.slice().reverse().join('');
+        image.setAttribute('src', newUrl);
+    } else {
+        image.setAttribute('src', newUrl);
+    }
+}
