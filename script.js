c9f16fa5ad25a884702122c592d63111f885c23a
pianistoff <severyn.h@gmail.com>
2022-03-17 18:33:49 +0200

    Make images with glass scrollable

--- script.js
+++ script.js
@@ -9,35 +9,35 @@ function menu() {
 
 function imageSwitchLeft(element) {
     const item = element.parentElement.parentElement;
-    const image = item.querySelector('img');
-    const url = image.getAttribute('src');
-    const arr = url.split('').reverse();
+    const image = item.querySelector("img");
+    const url = image.getAttribute("src");
+    const arr = url.split("").reverse();
     const pictureNumber = Number(arr[4]);
     arr[4] = (pictureNumber - 1).toString();
-    newUrl = arr.slice().reverse().join('');
+    newUrl = arr.slice().reverse().join("");
     if (Number(arr[4]) < 1) {
-        arr[4] = '6';
-        newUrl = arr.slice().reverse().join('');
-        image.setAttribute('src', newUrl);
+        arr[4] = "6";
+        newUrl = arr.slice().reverse().join("");
+        image.setAttribute("src", newUrl);
     } else {
-        image.setAttribute('src', newUrl);
+        image.setAttribute("src", newUrl);
     }
 }
 
 function imageSwitchRight(element) {
     const item = element.parentElement.parentElement;
-    const image = item.querySelector('img');
-    const url = image.getAttribute('src');
-    const arr = url.split('').reverse();
+    const image = item.querySelector("img");
+    const url = image.getAttribute("src");
+    const arr = url.split("").reverse();
     const pictureNumber = Number(arr[4]);
     arr[4] = (pictureNumber + 1).toString();
-    newUrl = arr.slice().reverse().join('');
+    newUrl = arr.slice().reverse().join("");
     if (Number(arr[4]) > 6) {
-        arr[4] = '1';
-        newUrl = arr.slice().reverse().join('');
-        image.setAttribute('src', newUrl);
+        arr[4] = "1";
+        newUrl = arr.slice().reverse().join("");
+        image.setAttribute("src", newUrl);
     } else {
-        image.setAttribute('src', newUrl);
+        image.setAttribute("src", newUrl);
     }
 }
 
@@ -56,7 +56,8 @@ function magnify(img, zoom) {
     /* Set background properties for the magnifier glass: */
     glass.style.backgroundImage = "url('" + img.src + "')";
     glass.style.backgroundRepeat = "no-repeat";
-    glass.style.backgroundSize = (img.width * zoom) + "px " + (img.height * zoom) + "px";
+    glass.style.backgroundSize =
+        img.width * zoom + "px " + img.height * zoom + "px";
     bw = 3;
     w = glass.offsetWidth / 2;
     h = glass.offsetHeight / 2;
@@ -70,26 +71,35 @@ function magnify(img, zoom) {
     img.addEventListener("touchmove", moveMagnifier);
     function moveMagnifier(e) {
         var pos, x, y;
-        /* Prevent any other actions that may occur when moving over the image */
-        e.preventDefault();
         /* Get the cursor's x and y positions: */
         pos = getCursorPos(e);
         x = pos.x;
         y = pos.y;
         /* Prevent the magnifier glass from being positioned outside the image: */
-        if (x > img.width - (w / zoom)) {x = img.width - (w / zoom);}
-        if (x < w / zoom) {x = w / zoom;}
-        if (y > img.height - (h / zoom)) {y = img.height - (h / zoom);}
-        if (y < h / zoom) {y = h / zoom;}
-        /* Set the position of the magnifier glass: */
-        glass.style.left = (x - w) + "px";
-        glass.style.top = (y - h) + "px";
-        /* Display what the magnifier glass "sees": */
-        glass.style.backgroundPosition = "-" + ((x * zoom) - w + bw) + "px -" + ((y * zoom) - h + bw) + "px";
+        if (x > img.width - w / zoom) {
+            x = img.width - w / zoom;
         }
+        if (x < w / zoom) {
+            x = w / zoom;
+        }
+        if (y > img.height - h / zoom) {
+            y = img.height - h / zoom;
+        }
+        if (y < h / zoom) {
+            y = h / zoom;
+        }
+        /* Set the position of the magnifier glass: */
+        glass.style.left = x - w + "px";
+        glass.style.top = y - h + "px";
+        /* Display what the magnifier glass "sees": */
+        glass.style.backgroundPosition =
+            "-" + (x * zoom - w + bw) + "px -" + (y * zoom - h + bw) + "px";
+    }
 
     function getCursorPos(e) {
-        var a, x = 0, y = 0;
+        var a,
+            x = 0,
+            y = 0;
         e = e || window.event;
         /* Get the x and y positions of the image: */
         a = img.getBoundingClientRect();
@@ -99,20 +109,23 @@ function magnify(img, zoom) {
         /* Consider any page scrolling: */
         x = x - window.pageXOffset;
         y = y - window.pageYOffset;
-        return {x : x, y : y};
-        }
+        return { x: x, y: y };
+    }
 }
 
 function zoomOut(element) {
-    const zoom = element.parentElement.parentElement.querySelector('.img-magnifier-glass')
+    const zoom = element.parentElement.parentElement.querySelector(
+        ".img-magnifier-glass"
+    );
     if (zoom) {
-    zoom.parentNode.removeChild(zoom);
+        zoom.parentNode.removeChild(zoom);
     }
- }
+}
 
 function glassSwitch(element) {
-    const toggle = element.parentElement.querySelector('.toggle');
-    const image = element.parentElement.parentElement.querySelector('.item-image');
+    const toggle = element.parentElement.querySelector(".toggle");
+    const image =
+        element.parentElement.parentElement.querySelector(".item-image");
     if (toggle.checked) {
         return zoomOut(element);
     } else {
