6e440ff1ef0ff959d33c5428903a0355a812921a
pianistoff <severyn.h@gmail.com>
2022-03-19 03:28:02 +0200

    Make glass appear at the right spot on first item images

--- script.js
+++ script.js
@@ -90,8 +90,9 @@ function magnify(img) {
             y = h / zoom;
         }
         /* Set the position of the magnifier glass: */
+        const distanceFromContainer = img.parentElement.getBoundingClientRect().top - img.getBoundingClientRect().top;
         glass.style.left = x - w + "px";
-        glass.style.top = y - h + "px";
+        glass.style.top = y - distanceFromContainer - h + "px";
         /* Display what the magnifier glass "sees": */
         glass.style.backgroundPosition =
             "-" + (x * zoom - w + bw) + "px -" + (y * zoom - h + bw) + "px";
