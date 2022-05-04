7eb5236321ee0dd0608d2f64f94825ea9eecf869
pianistoff <severyn.h@gmail.com>
2022-05-04 06:06:23 +0300

    [Desktop] Make resp arrows work

--- script.js
+++ script.js
@@ -4,21 +4,41 @@ const cardinalString =
     '<div class="cardinal item">\r\n    <h3>Puiforcat Cardinal Five-Piece Place Setting</h3>\r\n    <div class="image-container">\r\n        <img\r\n            class="item-image"\r\n            src="./images/cardinal/1.jpg"\r\n            alt=" "\r\n        />\r\n    </div>\r\n    <div class="buttons">\r\n        <a\r\n            href="javascript:void(0);"\r\n            onclick="imageSwitchLeft(this); arrowMagnifyOff(this);"\r\n            ><img\r\n                class="button arrow-left"\r\n                src="./images/buttons/arrow-left.png"\r\n                alt=""\r\n        /></a>\r\n        <a\r\n            href="javascript:void(0);"\r\n            onclick="imageSwitchRight(this); arrowMagnifyOff(this);"\r\n            ><img\r\n                class="button arrow-right"\r\n                src="./images/buttons/arrow-right.png"\r\n                alt=""\r\n        /></a>\r\n        <input type="checkbox" id="toggle2" class="toggle" />\r\n        <a\r\n            href="javascript:void(0);"\r\n            onclick="magnifySwitch(this)"\r\n            ><label for="toggle2"\r\n                ><img\r\n                    class="button glass"\r\n                    src="./images/buttons/glass.png"\r\n                    alt="" /></label\r\n        ></a>\r\n    </div>\r\n    <div class="text-block">\r\n        <p class="year">Year: 1925</p>\r\n        <p class="description-text">\r\n            Produced from a 17th-century drawing, this is a\r\n            pattern of simple forms and classic grace. This\r\n            setting\u2019s time-honored roots can be seen in such\r\n            elements as a the reinforced center tine of the fork\r\n            and a knife design that recalls the curve-bladed\r\n            style of old hunting knives.\r\n        </p>\r\n        <p class="description-text">\r\n            Silverplate. Includes dinner fork, dinner knife,\r\n            dessert spoon, salad fork and tea spoon. Also\r\n            available in sterling silver as the Richelieu\r\n            pattern.\r\n        </p>\r\n        <p class="price">$540.00</p>\r\n    </div>\r\n</div>';
 const chantacoString =
     '<div class="chantaco item">\r\n    <h3>Puiforcat Chantaco Five-Piece Place Setting</h3>\r\n    <div class="image-container">\r\n        <img\r\n            class="item-image"\r\n            src="./images/chantaco/1.jpg"\r\n            alt=" "\r\n        />\r\n    </div>\r\n    <div class="buttons">\r\n        <a\r\n            href="javascript:void(0);"\r\n            onclick="imageSwitchLeft(this); arrowMagnifyOff(this);"\r\n            ><img\r\n                class="button arrow-left"\r\n                src="./images/buttons/arrow-left.png"\r\n                alt=""\r\n        /></a>\r\n        <a\r\n            href="javascript:void(0);"\r\n            onclick="imageSwitchRight(this); arrowMagnifyOff(this);"\r\n            ><img\r\n                class="button arrow-right"\r\n                src="./images/buttons/arrow-right.png"\r\n                alt=""\r\n        /></a>\r\n        <input type="checkbox" id="toggle3" class="toggle" />\r\n        <a\r\n            href="javascript:void(0);"\r\n            onclick="magnifySwitch(this)"\r\n            ><label for="toggle3"\r\n                ><img\r\n                    class="button glass"\r\n                    src="./images/buttons/glass.png"\r\n                    alt="" /></label\r\n        ></a>\r\n    </div>\r\n    <div class="text-block">\r\n        <p class="year">Year: 1924</p>\r\n        <p class="description-text">\r\n            In this classic pattern, the clean, modern shape of\r\n            the utensils is balanced by the fluted handles\u2019\r\n            complex interplay of light and shadow.\r\n        </p>\r\n        <p class="description-text">\r\n            Silverplate. Includes dinner fork, dinner knife,\r\n            dessert spoon, salad fork and tea spoon.\r\n        </p>\r\n        <p class="price">$540.00</p>\r\n    </div>\r\n</div>';
-const currentString = cannesString;
-const arrowRespRight = '<svg class=\"arrow-resp arrow-resp-right\" xmlns=\"http:\/\/www.w3.org\/2000\/svg\" viewBox=\"0 0 256 512\"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https:\/\/fontawesome.com License - https:\/\/fontawesome.com\/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d=\"M118.6 105.4l128 127.1C252.9 239.6 256 247.8 256 255.1s-3.125 16.38-9.375 22.63l-128 127.1c-9.156 9.156-22.91 11.9-34.88 6.943S64 396.9 64 383.1V128c0-12.94 7.781-24.62 19.75-29.58S109.5 96.23 118.6 105.4z\"\/><\/svg>';
-const arrowRespLeft = '<svg class=\"arrow-resp arrow-resp-left\" xmlns=\"http:\/\/www.w3.org\/2000\/svg\" viewBox=\"0 0 256 512\"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https:\/\/fontawesome.com License - https:\/\/fontawesome.com\/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d=\"M137.4 406.6l-128-127.1C3.125 272.4 0 264.2 0 255.1s3.125-16.38 9.375-22.63l128-127.1c9.156-9.156 22.91-11.9 34.88-6.943S192 115.1 192 128v255.1c0 12.94-7.781 24.62-19.75 29.58S146.5 415.8 137.4 406.6z\"\/><\/svg>';
+let currentString = cannesString;
+const arrowRespRight =
+    '<a class="arrow-resp arrow-resp-right" href="javascript:void(0);" onclick="nextProduct(); items();"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M118.6 105.4l128 127.1C252.9 239.6 256 247.8 256 255.1s-3.125 16.38-9.375 22.63l-128 127.1c-9.156 9.156-22.91 11.9-34.88 6.943S64 396.9 64 383.1V128c0-12.94 7.781-24.62 19.75-29.58S109.5 96.23 118.6 105.4z"/></svg></a>';
+const arrowRespLeft =
+    '<a class="arrow-resp arrow-resp-left" href="javascript:void(0);" onclick="previousProduct(); items();"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M137.4 406.6l-128-127.1C3.125 272.4 0 264.2 0 255.1s3.125-16.38 9.375-22.63l128-127.1c9.156-9.156 22.91-11.9 34.88-6.943S192 115.1 192 128v255.1c0 12.94-7.781 24.62-19.75 29.58S146.5 415.8 137.4 406.6z"/></svg></a>';
 const description = document.querySelector(".description");
 const item = document.querySelector(".item");
 const cannes = document.querySelector(".cannes");
 const cardinal = document.querySelector(".cardinal");
 const chantaco = document.querySelector(".chantaco");
+function nextProduct() {
+    if (currentString === cannesString) {
+        currentString = cardinalString;
+    } else if (currentString === cardinalString) {
+        currentString = chantacoString;
+    } else if (currentString === chantacoString) {
+        currentString = cannesString;
+    }
+}
+function previousProduct() {
+    if (currentString === cannesString) {
+        currentString = chantacoString;
+    } else if (currentString === chantacoString) {
+        currentString = cardinalString;
+    } else if (currentString === cardinalString) {
+        currentString = cannesString;
+    }
+}
 function items() {
     if (window.matchMedia("(max-width: 961px)").matches) {
         description.innerHTML = cannesString + cardinalString + chantacoString;
     } else if (window.matchMedia("(min-width: 962px)").matches) {
         description.innerHTML = arrowRespLeft + currentString + arrowRespRight;
     }
-};
+}
 items();
 window.onresize = items;
 
